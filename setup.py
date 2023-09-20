bf3bf6522ecbae295d790909cc1fe6528e959ddb
darktyr <johnathon.gard@colorado.edu>
2023-09-20 09:09:08 -0600

    Updated the setup files to now use setup.cfg. The scripts will now be callable from the command line in the proper env

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-
-'''The setup script.'''
-
-from setuptools import setup, find_packages
-
-setup(
-    author='JDG, EEM',
-    author_email='johnathon.gard@colorado.edu',
-    python_requires='>=3.5',
-    description='SQUID Series Array Testing Script',
-    install_requires=['numpy', 'matplotlib', 'tqdm', 'pyserial', 'pyyaml'],
-    license='MIT license',
-    include_package_data=True,
-    keywords='SQUID',
-    name='SQUID_SSA_Char',
-    packages=['squid_ssa_char'],
-    test_suite='tests',
-    url='https://github.com/DarkTyr/squid_ssa_char',
-    version='0.0.0',
-    zip_safe=False,
-#    package_data={'dastardcommander': ['ui/*.ui', 'ui/*.png']},
-#    entry_points={
-#        'console_scripts': ['dcom=dastardcommander.dc:main'],
-#    },
-)
+# Minimal setup.py that will then use setup.cfg
+from setuptools import setup
+setup()
